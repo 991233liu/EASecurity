063cd59d54b6ac829a99e5e67d0acfbcead8420b
liulf <991233liu@163.com>
2021-06-28 18:26:16 +0800

    增加模块框架

---
+++ core/easecurity-core-all/src/main/java/com/easecurity/core/access/package-info.java
@@ -0,0 +1,6 @@
+/** Copyright © 2021-2050 刘路峰版权所有。 */
+/**
+ * 访问控制
+ *
+ */
+package com.easecurity.core.access;
---
+++ core/easecurity-core-all/src/main/java/com/easecurity/core/basis/package-info.java
@@ -0,0 +1,6 @@
+/** Copyright © 2021-2050 刘路峰版权所有。 */
+/**
+ * 基础：人员、组织、职务、角色、资源
+ *
+ */
+package com.easecurity.core.basis;
