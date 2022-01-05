d91e02e89e846b22a2708ae1eb4d42bad312b587
liulf <991233liu@163.com>
2022-01-05 13:58:36 +0800

    调整目录结构

---
+++ core/easecurity-core/src/main/java/com/easecurity/core/basis/package-info.java
@@ -0,0 +1,6 @@
+/** Copyright © 2021-2050 刘路峰版权所有。 */
+/**
+ * 所以实体类（bean），带‘Do’后缀的为业务实体，不带后缀的为数据库实体。业务实体全部放到com.easecurity.core.basis，数据库实体放到下级包中，包名以数据库表前缀区分。
+ *
+ */
+package com.easecurity.core.basis;
